6c0632b7b3b1a7eedbdbfd410b7350274c3eafed
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-21 15:17:47 +0800

    +update

--- polygon_point_module/PolygonPointNode.cpp
+++ polygon_point_module/PolygonPointNode.cpp
@@ -1,18 +1,106 @@
 ﻿#include "PolygonPointNode.hpp"
+#include <Static.hpp>
 
+namespace sstd {
 
+    PolygonPointNodeData::PolygonPointNodeData() {
+        thisWidth = 1;
+        thisHeight = 1;
+        thisPolygonSize = 3;
+        thisFlags.setAll();
+    }
 
+    PolygonPointNode::PolygonPointNode(std::shared_ptr<PolygonPointNodeData> arg)
+        :thisData(arg) {
+    }
 
+    PolygonPointDrawNode::PolygonPointDrawNode(std::shared_ptr<QSGGeometry> arg) :
+        thisData(std::move(arg)) {
+        this->setGeometry(thisData.get());
+        this->setMaterial(&thisMaterial);
+    }
 
+    void PolygonPointDrawNode::setColor(const QColor & arg) {
+        thisMaterial.setColor(arg);
+        this->markDirty(DirtyMaterial);
+    }
 
+    void PolygonPointNode::updateTheNode() {
 
+        if (thisData->testChanged< PolygonPointNodeDataState::SizeChanged >()) {
+            auto varStatic = static_cast<Static*>(Static::instance(nullptr, nullptr));
+            auto varNode =
+                sstd_new<PolygonPointDrawNode>(varStatic->getPolygon(thisData->getPolygonSize()));
+            if (thisDrawNode) {
+                this->removeChildNode(thisDrawNode);
+            }
+            thisDrawNode = varNode;
+            this->appendChildNode(thisDrawNode);
+            varNode->setColor(thisData->getColor());
+        }
 
+        if (thisData->testChanged< PolygonPointNodeDataState::ColorChanged >()) {
+            thisDrawNode->setColor(thisData->getColor());
+        }
 
+        if ((thisData->testChanged< PolygonPointNodeDataState::CenterChanged >()) ||
+            (thisData->testChanged< PolygonPointNodeDataState::RadiusChanged >())) {
 
+            auto varRadius = thisData->getRadius();
+            auto varCenter = thisData->getCenter();
 
+            QMatrix4x4 varMatrix;
+            varMatrix.translate(varRadius.first*0.5, varRadius.second*0.5);
+            varMatrix.scale(varRadius.first*0.5, -varRadius.second*0.5);
+            varMatrix.translate(-varCenter.x(), -varCenter.y());
 
+            this->setMatrix(varMatrix);
+            this->markDirty(DirtyMatrix);
+        }
 
+        thisData->clearAllChange();
 
+    }
 
+    void PolygonPointNodeData::clearAllChange() {
+        thisFlags.clearAll();
+    }
 
+    bool PolygonPointNodeData::setPolygonSize(const int &arg) {
+        if (arg == thisPolygonSize) {
+            return false;
+        }
+        thisPolygonSize = arg;
+        thisFlags.set< PolygonPointNodeDataState::SizeChanged >();
+        return true;
+    }
 
+    bool PolygonPointNodeData::setRadius(const double &argWidth, const double &argHeight) {
+        if ((argWidth == thisWidth) && (argHeight == thisHeight)) {
+            return false;
+        }
+        thisWidth = argWidth;
+        thisHeight = argHeight;
+        thisFlags.set< PolygonPointNodeDataState::RadiusChanged >();
+        return true;
+    }
+
+    bool PolygonPointNodeData::setCenter(const QPointF &arg) {
+        if (arg == thisCenter) {
+            return false;
+        }
+        thisCenter = arg;
+        thisFlags.set< PolygonPointNodeDataState::CenterChanged >();
+        return true;
+    }
+
+    bool PolygonPointNodeData::setColor(const QColor & arg) {
+        if (arg == thisColor) {
+            return false;
+        }
+        thisColor = arg;
+        thisFlags.set< PolygonPointNodeDataState::ColorChanged >();
+        return true;
+    }
+
+}/*namespace sstd*/
--- polygon_point_module/PolygonPointNode.hpp
+++ polygon_point_module/PolygonPointNode.hpp
@@ -4,9 +4,87 @@
 
 namespace sstd{
 
-    class PolygonPointNode : public QSGNode {
-
+    enum class PolygonPointNodeDataState{
+        SizeChanged,
+        RadiusChanged,
+        CenterChanged,
+        ColorChanged,
+        Size
     };
+
+    class PolygonPointNodeData {
+    public:
+        PolygonPointNodeData();
+    public:
+        void clearAllChange();
+    public:
+        inline int getPolygonSize() const ;
+        inline std::pair<double,double> getRadius() const;
+        inline QPointF getCenter() const;
+        inline QColor getColor() const;
+    public:
+        bool setPolygonSize(const int &);
+        bool setRadius(const double &,const double &);
+        bool setCenter(const QPointF &);
+        bool setColor(const QColor &);
+    public:
+        template<PolygonPointNodeDataState>
+        inline bool testChanged() const;
+    private:
+        int thisPolygonSize;
+        double thisWidth;
+        double thisHeight;
+        QPointF thisCenter;
+        QColor thisColor;
+        sstd::QuickFlags< PolygonPointNodeDataState::Size > thisFlags;
+    private:
+        sstd_class(PolygonPointNodeData);
+    };
+
+    class PolygonPointDrawNode : public QSGGeometryNode {
+    public:
+        PolygonPointDrawNode(std::shared_ptr<QSGGeometry>);
+    public:
+        void setColor(const QColor &);
+    private:
+        std::shared_ptr<QSGGeometry> thisData;
+        QSGFlatColorMaterial thisMaterial;
+    private:
+        sstd_class(PolygonPointDrawNode);
+    };
+
+    class PolygonPointNode : public QSGTransformNode {
+    public:
+        PolygonPointNode(std::shared_ptr<PolygonPointNodeData>);
+    public:
+        void updateTheNode();
+    private:
+        std::shared_ptr<PolygonPointNodeData> thisData;
+        PolygonPointDrawNode * thisDrawNode{nullptr};
+    private:
+        sstd_class(PolygonPointNode);
+    };
+
+    inline int PolygonPointNodeData::getPolygonSize() const {
+        return thisPolygonSize;
+    }
+
+    inline std::pair<double, double> PolygonPointNodeData::getRadius() const {
+        return { thisWidth,thisHeight };
+    }
+
+    inline QPointF PolygonPointNodeData::getCenter() const {
+        return thisCenter;
+    }
+
+    inline QColor PolygonPointNodeData::getColor() const {
+        return thisColor;
+    }
+
+    template<PolygonPointNodeDataState I>
+    inline bool PolygonPointNodeData::testChanged() const {
+        return thisFlags.test<I>();
+    }
 
 }/*namespace sstd*/
 
